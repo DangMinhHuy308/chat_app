37dde3fdac42f48585a3dc63ee09d2c2ffbacf9c
DangMinhHuy308 <huyminhdang5@gmail.com>
2024-05-11 20:56:26 +0700

    register & logout completed

--- api/server.js
+++ api/server.js
@@ -13,7 +13,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(express.json());
